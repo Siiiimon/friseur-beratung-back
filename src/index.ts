53a0ce52bdae7d0cfc95bab263685f2584b9fe03
Simon Cornelius <simonjoecornelius@gmail.com>
2025-06-27 04:29:15 +0200

    feat: add postgres db setup and schema for shampoos and conditioners

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,27 @@
 import express from "express";
 import dotenv from "dotenv";
 import apiRoutes from "./routes/routes";
+import { Client } from "pg";
 
 dotenv.config();
 
 const app = express();
-const port = process.env.PORT || 3000;
+const client = new Client();
 
-app.use(express.json());
-app.use("/", apiRoutes);
+const init = async () => {
+    await client.connect();
+    app.locals.pg = client;
+
+    const port = process.env.PORT || 3000;
+
+    app.use(express.json());
+    app.use("/", apiRoutes);
+
+    app.listen(port, () => {
+        console.log(`Server running on port ${port}`);
+    });
+};
+
+init();
 
 export default app;
-
-app.listen(port, () => {
-    console.log(`Server running on port ${port}`);
-});
---
+++ src/types/recommendation.ts
@@ -0,0 +1,8 @@
+export type Product = {
+    name: string;
+    tag: string;
+    type: string;
+    description: string;
+};
+
+export type Recommendation = Product[];
